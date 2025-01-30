258640986d57ab28a878c59428d0b17adf438daf
GeorgiosKallarogiannis <g.kallarogiannis@its-stuttgart.de>
2025-01-30 23:01:08 +0100

    Create a function that returns a random string
    The string the getComputerChoice() function returns at random, will
    be used to compete against.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-rock-paper-scissors
+A simple rock-paper-scissors game.
+This game is played exclusively within the console.
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("!");
+function getComputerChoice() {
+    const randomNum = Math.random();
+    const roundedNum = Math.floor(randomNum * 100);
+
+    if (roundedNum >= 0 && roundedNum < 33) {
+        return "rock";
+    } else if (roundedNum > 32 && roundedNum < 66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
