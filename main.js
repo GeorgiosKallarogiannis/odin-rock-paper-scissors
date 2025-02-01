118dd417b52691640b12150ae13e8ebda1251568
GeorgiosKallarogiannis <g.kallarogiannis@its-stuttgart.de>
2025-02-01 16:38:44 +0100

    Add a loop function
    The playGame() function, repeats our game exactly five times. It than
    compares two values and console logs an appropriate message.

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     const randomNum = Math.random();
     const roundedNum = Math.floor(randomNum * 100);
@@ -18,21 +15,34 @@ function getHumanChoice() {
     return prompt().toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-    if ((humanChoice === "paper" && computerChoice === "rock") || 
-        (humanChoice === "scissors" && computerChoice === "paper") ||
-        (humanChoice === "rock" && computerChoice === "scissors")) {
-        humanScore++;
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-    } else if (humanChoice === computerChoice) {
-        console.log("It's a draw!");
-        return;
-    } else {
-        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore++;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if ((humanChoice === "paper" && computerChoice === "rock") || 
+            (humanChoice === "scissors" && computerChoice === "paper") ||
+            (humanChoice === "rock" && computerChoice === "scissors")) {
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        } else if (humanChoice === computerChoice) {
+            console.log("It's a draw!");
+            return;
+        } else {
+            console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
     }
 
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Congratulations, you win!");
+    } else {
+        console.log("You sadly lost. Better luck next time!");
+    }
 }
 
-playRound(getHumanChoice(), getComputerChoice());
-playRound(getHumanChoice(), getComputerChoice());
+playGame();
