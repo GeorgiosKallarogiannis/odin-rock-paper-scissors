c38595f2bcaf380003f994deaa91f92b76de26b9
GeorgiosKallarogiannis <g.kallarogiannis@its-stuttgart.de>
2025-02-13 21:42:52 +0100

    Add event listeners to the project
    Instead of relying on a loop to continue the game,
    we added buttons and are now listening to the click
    events, to call the necessary function.

--- main.js
+++ main.js
@@ -19,29 +19,53 @@ function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
+    const rock = document.querySelector("#rock");
+    const paper = document.querySelector("#paper");
+    const scissors = document.querySelector("#scissors");
+    const resultContainer = document.querySelector("#result")
+    const humanPara = document.createElement("p");
+    const compPara = document.createElement("p");
+    const resultPara = document.createElement("p");
+
+    humanPara.textContent = `Human Score: ${humanScore}`;
+    compPara.textContent = `Human Score: ${computerScore}`;
+
+    resultContainer.appendChild(humanPara);
+    resultContainer.appendChild(compPara);
+    resultContainer.appendChild(resultPara);
+
+    rock.addEventListener("click", () => playRound("rock", getComputerChoice()));
+    paper.addEventListener("click", () => playRound("paper", getComputerChoice()));
+    scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()));
+
     function playRound(humanChoice, computerChoice) {
-        if ((humanChoice === "paper" && computerChoice === "rock") || 
+        if ((humanChoice === "paper" && computerChoice === "rock") ||
             (humanChoice === "scissors" && computerChoice === "paper") ||
             (humanChoice === "rock" && computerChoice === "scissors")) {
             humanScore++;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+
+            humanPara.textContent = `Human Score: ${humanScore}`;
+
+            resultPara.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
         } else if (humanChoice === computerChoice) {
-            console.log("It's a draw!");
+            resultPara.textContent = "It's a draw!";
             return;
         } else {
-            console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
             computerScore++;
+
+            compPara.textContent = `Human Score: ${computerScore}`;
+
+            resultPara.textContent =`You Lose! ${computerChoice} beats ${humanChoice}.`;
         }
-    }
 
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (humanScore > computerScore) {
-        console.log("Congratulations, you win!");
-    } else {
-        console.log("You sadly lost. Better luck next time!");
+        switch (5) {
+            case computerScore:
+                resultPara.textContent = "You sadly lost. Better luck next time!";
+                break;
+            case humanScore:
+                resultPara.textContent = "Congratulations, you win!";
+                break;
+        }
     }
 }
 
